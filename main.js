035a0a1561ac26258b73905fbd525c0ec18e9086
Susan Thai <susanthai@icloud.com>
2020-09-14 01:10:27 -0400

    almost finished
    Just need to make it pretty and cleaner

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 const celsiusInput = document.querySelector('#celcius > input');
 const fahrenheitInput = document.querySelector('#fahrenheit > input');
 
+function roundNum(num){
+    return Math.round(num*100)/100;
+}
+
 function cToF(){
     const cTemp = parseFloat(celsiusInput.value);
     const fTemp = (cTemp * (9/5)) + 32;
 
-    fahrenheitInput,value = fTemp; 
+    fahrenheitInput.value = roundNum(fTemp); 
 }
 
 function fToC(){
     const fTemp = parseFloat(fahrenheitInput.value);
-    console.log(fTemp);
+    const cTemp = (fTemp - 32) * (5/9);
+    
+    celsiusInput.value = roundNum(cTemp);
 }
 
+function main(){
 celsiusInput.addEventListener('input', cToF);
-// fahrenheitInput.addEventListener('input', fToC);
+fahrenheitInput.addEventListener('input',fToC);
+}
+
+main();
 
   
